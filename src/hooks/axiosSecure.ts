30b49f20adb5a2b56c901af7bef524b96045d7d1
Captain-Kanak <kanakroy835@gmail.com>
2026-01-22 19:55:07 +0600

    axios instace and tanstack query setup done

---
+++ src/hooks/axiosSecure.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { getToken } from "../helpers/localStorage";
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
+
+const axiosSecure = () => {
+  instance.interceptors.request.use(
+    async (config) => {
+      const token = getToken();
+
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
+};
+
+export default axiosSecure;
